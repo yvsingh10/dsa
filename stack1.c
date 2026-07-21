098d24c8719f6ffd0a662bfd07eba68a0273aff7
yvsingh10 <2025442587.yash@ug.sharda.ac.in>
2026-07-21 10:39:54 +0530

    Create stack1.c

---
+++ stack1.c
@@ -0,0 +1,70 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+    void push(int x);
+    void pop();
+    void peek();
+    void traverse ();
+    int top= -1;
+    int st[4];
+    
+    
+int main() {
+    
+    push(9);
+    push(6);
+    push(5);
+    push(10);
+    pop();
+    push(1);
+    peek();
+    traverse ();
+   
+    return 0;
+}
+
+void push(int x){
+    if (top==3){
+        printf("Stack is full");
+        
+        
+    }
+    else{
+        top=top+1;
+        st[top]=x;
+    }
+        
+    }
+    
+    
+    void pop(){
+        if(top==-1){
+            printf("stack is alrerady empty");
+            
+        }
+        else{
+            int y;
+            y= st[top];
+            top=top-1;
+        }
+        }
+
+void peek(){
+    int y;
+    y= st[top];
+    printf("value of top is %d\n",y);
+}
+
+void traverse()
+{
+    if(top==-1){
+        printf("stack is empty");
+    }
+    else{
+        printf("stack elements= ");
+        for(int i=top;i>=0;i--){
+            printf("%d\n",st[i]);
+        }
+        }
+        printf("\n");
+    }
+    
